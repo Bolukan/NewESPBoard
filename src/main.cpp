f59596afe896d1215b58fbb6b7db9d8090f81f34
ESP <github@bolukan.nl>
2018-06-07 23:37:14 +0200

    Add Flash Chip

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "secrets.h"
+#include <locale.h>
 
 #ifndef SECRETS_H
  #define SECRETS_H
@@ -13,10 +14,10 @@ void setup(){
    Serial.begin(115200);
    while (!Serial) { delay(1); }
 
-   Serial.println("Congratulations with your new ESP Board!!");
+   setlocale(LC_ALL, "nl_NL.UTF8");
 
-   Serial.print("MAC: ");
-   Serial.println(WiFi.macAddress());
+   Serial.println();
+   Serial.println("Congratulations with your new ESP Board!!");
 
    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
    while (WiFi.status() != WL_CONNECTED) {
@@ -24,12 +25,32 @@ void setup(){
      Serial.println("Connecting to WiFi..");
    }
 
+   Serial.println("\nWIFI");
+
+   Serial.print("MAC: ");
+   Serial.println(WiFi.macAddress());
+
    Serial.print("WiFi IP: ");
    Serial.println(WiFi.localIP());
+
+   Serial.print("ESP Chip Id: ");
+   Serial.printf("0x%x\n", ESP.getChipId());
+
+   Serial.print("SDK version: ");
+   Serial.println(ESP.getSdkVersion());
+
+   Serial.println("\nFLASH");
+   Serial.printf("Chip ID: 0x%x\n", ESP.getFlashChipId());
+   Serial.printf("Chip Real Size: %u bytes\n", ESP.getFlashChipRealSize());
+   Serial.printf("Chip Size: %u bytes\n", ESP.getFlashChipSize());
+   Serial.printf("Chip Speed: %u Hz\n", ESP.getFlashChipSpeed());
+
+   Serial.print("Chip Mode (0=QIO;1=QOUT;2=DIO;3=DOUT;255=unknown): ");
+   Serial.println(ESP.getFlashChipMode());
 
 }
 
 void loop()
 {
-
+    delay(1);
 }
