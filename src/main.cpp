22e3d857a5d17eb3b6d9d567dc53064a004b20ca
ESP <github@bolukan.nl>
2018-07-28 20:52:21 +0200

    Reorder info

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,49 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include "secrets.h"
-#include <locale.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+#endif
 
+#include "secrets.h"
 #ifndef SECRETS_H
  #define SECRETS_H
- const char* WIFI_SSID     = "ssid";
- const char* WIFI_PASSWORD = "password";
+ char WIFI_SSID[]     = "ssid";
+ char WIFI_PASSWORD[] = "password";
 #endif
 
 void setup(){
 
    Serial.begin(115200);
-   while (!Serial) { delay(1); }
-
-   setlocale(LC_ALL, "nl_NL.UTF8");
 
    Serial.println();
    Serial.println("Congratulations with your new ESP Board!!");
 
+   Serial.print("\nCHIP\n");
+   Serial.printf("ESP Chip Id:    0x%06x\n", ESP.getChipId());
+   Serial.printf("Cpu Freq:       %u MHz\n", ESP.getCpuFreqMHz());
+
+   Serial.print("\nFLASH\n");
+   Serial.printf("Chip ID :       0x%06x\n", ESP.getFlashChipId());
+   Serial.printf("Chip Size:      %u bytes\n", ESP.getFlashChipSize());
+   Serial.printf("Chip Real Size: %u bytes\n", ESP.getFlashChipRealSize());
+   Serial.printf("Chip Speed:     %u Hz\n", ESP.getFlashChipSpeed());
+   FlashMode_t ideMode = ESP.getFlashChipMode();
+   Serial.printf("Chip Mode:      0x%02x %s\n", ideMode, (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT" : ideMode == FM_DIO ? "DIO" : ideMode == FM_DOUT ? "DOUT" : "UNKNOWN"));
+
+   Serial.print("\nSOFTWARE\n");
+   Serial.printf("SDK version:    %s\n", ESP.getSdkVersion());
+   Serial.printf("Core version:   %s\n", ESP.getCoreVersion().c_str());
+
+   Serial.print("\nWIFI\n");
+   Serial.print("Connecting to WiFi .");
    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
    while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.println("Connecting to WiFi..");
+     delay(250);
+     Serial.print(".");
    }
-
-   Serial.println("\nWIFI");
-
-   Serial.print("MAC: ");
-   Serial.println(WiFi.macAddress());
-
-   Serial.print("WiFi IP: ");
-   Serial.println(WiFi.localIP());
-
-   Serial.print("ESP Chip Id: ");
-   Serial.printf("0x%x\n", ESP.getChipId());
-
-   Serial.print("SDK version: ");
-   Serial.println(ESP.getSdkVersion());
-
-   Serial.println("\nFLASH");
-   Serial.printf("Chip ID: 0x%x\n", ESP.getFlashChipId());
-   Serial.printf("Chip Real Size: %u bytes\n", ESP.getFlashChipRealSize());
-   Serial.printf("Chip Size: %u bytes\n", ESP.getFlashChipSize());
-   Serial.printf("Chip Speed: %u Hz\n", ESP.getFlashChipSpeed());
-
-   Serial.print("Chip Mode (0=QIO;1=QOUT;2=DIO;3=DOUT;255=unknown): ");
-   Serial.println(ESP.getFlashChipMode());
+   Serial.print("\n");
+   Serial.printf("MAC:            %s\n", WiFi.macAddress().c_str());
+   Serial.printf("WiFi IP:        %s\n", WiFi.localIP().toString().c_str());
 
 }
 
