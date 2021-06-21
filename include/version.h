9a4c20e1c1e491fd29cb7266e38bb743b98e8d8b
unknown <github@bolukan.nl>
2021-06-22 01:11:36 +0200

    Add Version as example

---
+++ include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "93"
+#endif
+#ifndef VERSION
+  #define VERSION "v1.0.210622_93 - 2021-06-22 00:31:29.328541"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v1.0.210622_93"
+#endif
